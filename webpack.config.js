c3c805a76958d828806e8c1b6a2f567963a377b0
Thota Tarun (Digital) <TH351984@wipro.com>
2018-08-02 17:04:35 +0530

    Full Code Commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var config = {
+   entry: './main.js',
+   output: {
+      path:__dirname+'/',
+      filename: 'index.js',
+   },
+   devServer: {
+      inline: true,
+      port: 8181
+   },
+   module: {
+      loaders: [
+         {
+            test: [/\.jsx?$/,/\.js?$/],
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+module.exports = config;
